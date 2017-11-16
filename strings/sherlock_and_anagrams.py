480be292a93c3f5dd74b67eeb1dcf0f5c11fafed
Simone <simone.procaccia@gmail.com>
2017-11-16 12:19:59 +0100

    sherlock to finish

---
+++ strings/sherlock_and_anagrams.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def substring_bitch(s):
+    for i in range(len(s)):
+        pass
+
+def sherlock_and_anagrams(s):
+
+
+
+q = int(input().strip())
+for a0 in range(q):
+    s = input().strip()
+    result = sherlock_and_anagrams(s)
+    print(result)
